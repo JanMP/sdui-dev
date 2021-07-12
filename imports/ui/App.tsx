26b89a6c67a22f78cabfcf0aab150f92e7c781b9
JanMP <pille@mac.com>
2021-07-12 15:20:12 +0200

    handover

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {MeteorDataAutoTable} from 'meteor/janmp:sdui-table';
-import {props} from '/imports/api/AutoTableTest';
+import { MeteorDataAutoTable } from 'meteor/janmp:sdui-table';
+import { LoginForm } from 'meteor/janmp:sdui-roles';
+import { props } from '/imports/api/AutoTableTest';
 
-console.log({props})
+console.log({ props });
 
 export const App = () => (
-    <div className="h-screen">
-      <MeteorDataAutoTable {...props}/>
+  <div className="flex justify-center pt-5">
+    <div className="w-full md:w-1/2 text-center">
+      <LoginForm />
     </div>
+  </div>
 );
