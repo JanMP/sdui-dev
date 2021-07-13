bc53a5d54590d202b0d143dca97cc164b887e5f5
JanMP <pille@mac.com>
2021-07-13 10:36:23 +0200

    add accounts-pasword

--- client/main.tsx
+++ client/main.tsx
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base'
 import React from 'react';
 import { render } from 'react-dom';
 
@@ -7,8 +8,11 @@ import '/imports/ui/main'
 import '/imports/ui/fixedReactVirtualized'
 import { App } from '/imports/ui/App'
 import FormTest from '/imports/ui/FormTest'
+
 import Modal from 'react-modal'
 Modal.setAppElement('#react-target')
+
+Accounts.onResetPasswordLink((token, done) => console.log({token}))
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('react-target'));
