74afea30dae323a2cdf5629ad6b9283c3fdce7fc
Jan Pilgenröder <pille@mac.com>
2021-11-23 17:54:38 +0100

    move scaffolder folder out of repo

--- scaffolder/cs-component/{{componentName}}.coffee
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-
-{{componentName}} = ->
-  <div className="m-1 p-2 rounded border border-red-300">{{componentName}}</div>
--- scaffolder/ts-compoment/{{componentName}}.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const {{componentName}} = () => {
-  return (
-    <div className="m-1 p-2 rounded border border-red-300" >[{{componentName}}]</div>
-  )
-}
-
-export default {{componentName}};
